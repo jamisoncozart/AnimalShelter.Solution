d661878d33bdd076e1644d15b0c453e44b2154fe
Katy Solovewicz <katy@solovewicz.com>
2020-03-17 11:23:24 -0700

    fix bug so primary key is recognized

--- AnimalShelter/Models/Animal.cs
+++ AnimalShelter/Models/Animal.cs
@@ -4,24 +4,10 @@ namespace AnimalShelter.Models
 {
   public class Animal
   {
-    public int IdAnimals { get; set; }
+    public int AnimalId { get; set; }
     public string Name {get; set;}
     public string Gender {get; set;}
     public DateTime DateAdmitted {get; set;}
     public string Breed {get; set;}
-
-    
-    // public Animal(string name, string gender, DateTime dateAdmitted, string breed)
-    // {
-    //   Name = name;
-    //   Gender = gender;
-    //   DateAdmitted = dateAdmitted;
-    //   Breed = breed;
-    // }
-    // public static List<Animal> GetAll()
-    // {
-    
-    // }
-    
   }
 }
