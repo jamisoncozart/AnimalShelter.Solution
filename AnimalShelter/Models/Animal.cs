1f949551f50459235722ca522215394dbc99f865
Jamison Cozart <jamisoncozart@gmail.com>
2020-03-17 09:37:02 -0700

    Initial Commit
    Co-authored-by: Katy Solovewicz <katy@solovewicz.com>
    Co-authored-by: Jeremy Kale Padot <jeremy@padot.us>

---
+++ AnimalShelter/Models/Animal.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace AnimalShelter.Models
+{
+  public class Animal
+  {
+    public int AnimalId { get; set; }
+    public string Name {get; set;}
+    public string Gender {get; set;}
+    public DateTime DateAdmitted {get; set;}
+    public string Breed {get; set;}
+
+    
+    // public Animal(string name, string gender, DateTime dateAdmitted, string breed)
+    // {
+    //   Name = name;
+    //   Gender = gender;
+    //   DateAdmitted = dateAdmitted;
+    //   Breed = breed;
+    // }
+    // public static List<Animal> GetAll()
+    // {
+    
+    // }
+    
+  }
+}
