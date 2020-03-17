09ad7aca0f10d2e397255eda78422303566a01a6
Katy Solovewicz <katy@solovewicz.com>
2020-03-17 15:14:44 -0700

    add edit route to animalscontroller
    Co-authored-by: Jeremy Kale Padot <jeremy@padot.us>
    Co-authored-by: Jamison Cozart <jamisoncozart@gmail.com>

--- AnimalShelter/Controllers/AnimalsController.cs
+++ AnimalShelter/Controllers/AnimalsController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using AnimalShelter.Models;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace AnimalShelter.Controllers
 {
@@ -41,7 +42,19 @@ namespace AnimalShelter.Controllers
       Animal thisAnimal = _db.Animals.FirstOrDefault(animals => animals.AnimalId == id);
       return View(thisAnimal);
     }
+    public ActionResult Edit(int id)
+    {
+        var thisAnimal = _db.Animals.FirstOrDefault(animals => animals.AnimalId == id);
+        return View(thisAnimal);
+    }
 
+    [HttpPost]
+    public ActionResult Edit(Animal animal)
+    {
+        _db.Entry(animal).State = EntityState.Modified;
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+    }
     // public ActionResult ShowSorted(string searchTerm)
     // {
     //   List<Animal> model = _db.Animals.ToList();
